4d635dd9bf12816badb4940d6809e3e57fd275c2
Claudisnei Alves <claudisneibello@gmail.com>
2021-12-30 00:05:18 -0300

    Se adaptando a estrutura dos controllers

--- API_REST_USUARIOS/database/connection.js
+++ API_REST_USUARIOS/database/connection.js
@@ -1,10 +1,10 @@
 var knex = require('knex')({
-    client: 'mysql2',
+    client: 'Mysql_curso',
     connection: {
       host : '127.0.0.1',
       user : 'root',
-      password : '',
-      database : 'usertest'
+      password : 'root',
+      database : 'apiusers'
     }
   });
 
