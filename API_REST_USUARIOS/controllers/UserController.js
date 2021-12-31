b199e84e1491f3f81697eb5044068ed2c6075661
Claudisnei Alves <claudisneibello@gmail.com>
2021-12-31 20:42:51 -0300

    Instalado o bcrypt para hash de senha, Criando método findEmail para verificar se o email já existe, salvando o usuário no banco de dados.

--- API_REST_USUARIOS/controllers/UserController.js
+++ API_REST_USUARIOS/controllers/UserController.js
@@ -1,25 +1,45 @@
 var User = require("../models/User");
 
 class UserController{
+
     async index(req,res){}
+
     async create(req, res){
+
         //console.log(req.body);
         //res.send("Pegando o corpo da requisição");
+
          var {email, name, password} = req.body;
+
        if(email == undefined || email == "" || email == " "){
+
             res.status(400);
             res.json({err: "O email é inválido"});
             return;
+
         }else if( name == undefined || name == " " || name == ""){
+
             res.status(400);
             res.json({err: "O nome é inválido"});
             return;
+
         }else{
             try {
+
+                var emailExists = await User.findEmail(email);
+
+                if(emailExists){
+                    res.status(406);
+                    res.json({err: "O email já está cadastrado!"});
+                    return;
+                }
+
                 await User.new(email,name,password);
+
                 res.status(200);
                 // console.log(req.body);
                  res.send("Tudo OK!");
+
             } catch (error) {
                 console.log(error);
             }       
--- API_REST_USUARIOS/models/User.js
+++ API_REST_USUARIOS/models/User.js
@@ -1,13 +1,35 @@
 var Knex = require("../database/connection");
+var bcrypt = require("bcrypt");
+const knex = require("../database/connection");
 
 class User {
 
     async new(email,name,password){
         try {
-            await Knex.insert({email,name,password,role: 0}).table('users')
+            var hash = await bcrypt.hash(password,10);
+
+            await Knex.insert({email, name, password: hash, role: 0}).table('users');
+
         } catch (error) {
             console.log(error);
         }
+    }
+
+    async findEmail(email){
+        try {
+
+            var result = await knex.select("*").from("users").where({email: email});
+
+            if(result.length > 0){
+                return true;
+            }else{
+                return false;
+            }
+
+        } catch (error) {
+            console.log(error);
+            return false;
+        }  
     }
 }
 
