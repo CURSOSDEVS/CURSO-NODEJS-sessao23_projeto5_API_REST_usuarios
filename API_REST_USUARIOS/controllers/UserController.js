31229ca3c3390da3b0ce07f06bd8e55b2d6599f6
Claudisnei Alves <claudisneibello@gmail.com>
2021-12-30 22:36:53 -0300

    Validacao dos dados passados pela requisicao

--- API_REST_USUARIOS/controllers/UserController.js
+++ API_REST_USUARIOS/controllers/UserController.js
@@ -4,10 +4,17 @@ class UserController{
     async index(req, res){}
 
     async create(req, res){
-        console.log(req.body);
-        res.send("Pegando o corpo da requisição");
+        //fazendo um destruct para capturar os dados enviados na requisição
+        var{email, name, password} = req.body;
+        //validações dos campos
+        if(email == undefined || email == ""){
+            res.status(400);
+            res.json({err:"O email é inválido"});
+        }else{
+            res.status(200);
+            res.send("Tudo Ok!")
+        }   
     }
-
 }
 
 module.exports = new UserController();
--- API_REST_USUARIOS/index.js
+++ API_REST_USUARIOS/index.js
@@ -4,11 +4,12 @@ var app = express()
 var router = require("./routes/routes")
  
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }))
 // parse application/json
-app.use(bodyParser.json())
+app.use(express.json())
 
 app.use("/",router);
+
 
 app.listen(8686,() => {
     console.log("Servidor rodando")
